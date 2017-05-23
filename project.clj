2d607af5bf94271e600abc1587031c8413212f52
ChalkPE <chalk@chalk.pe>
2017-05-23 18:17:51 +0900

    Updated information

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject game-shop "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "The answer of utilforever/ProgrammingPractice: Game Shop, Part 1"
+  :url "https://github.com/ChalkPE/programming-practice-game-shop"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
